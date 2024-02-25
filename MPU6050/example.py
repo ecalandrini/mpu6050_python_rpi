df6b46dc8c0a24121101e88184f83681ff59b046
Eugenio Calandrini <98228452+ecalandrini@users.noreply.github.com>
2024-02-25 19:32:01 +0100

    Update example.py

--- MPU6050/example.py
+++ MPU6050/example.py
@@ -6,6 +6,7 @@ Created on Sun Feb 25 16:57:55 2024
 """
 
 from mpu6050 import MPU6050
+import numpy as np
 
 sensor = MPU6050(0x68)
 sensor.wakeup()
@@ -15,9 +16,9 @@ sensor.config_get()
 sensor.gyro_config_get()
 sensor.accel_config_get()
 
-t = sensor.temp_get()
-gy = sensor.gyro_get()
-a = sensor.accel_get()
+t = []
+gy = []
+a = []
 
 for i in range(200):
     print(i)
@@ -25,9 +26,9 @@ for i in range(200):
     sensor.gyro_get()
     sensor.accel_get()
     if i >= 100:
-        t += sensor.temp
-        gy += sensor.gyro
-        a += sensor.accel
+        t.append(sensor.temp)
+        gy.append(sensor.gyro)
+        a.append(sensor.accel)
         
-print("values", t, gy, a)
-print("mean", t/100, gy/100, a/100)
+print("values", len(t), len(gy), len(a))
+print("mean", np.array(t).mean(), np.array(gy).mean(), np.array(a).mean())
