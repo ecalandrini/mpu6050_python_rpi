20d8fd93b3491e04d0106f9049a1968c746f5053
Eugenio Calandrini <98228452+ecalandrini@users.noreply.github.com>
2024-03-23 12:03:12 +0100

    test

--- HMC5883L/register_map.py
+++ HMC5883L/register_map.py
@@ -10,108 +10,44 @@ Created on Thu Feb 22 21:24:10 2024
 # =============================================================================
 
 class RegisterMap:
-    SELF_TEST_X = 0x0D
-    SELF_TEST_Y = 0x0E
-    SELF_TEST_Z = 0x0F
-    SELF_TEST_A = 0x10
-    
-    SMPLRT_DIV = 0x19
-    
-    CONFIG = 0x1A
-    GYRO_CONFIG = 0x1B 
-    ACCEL_CONFIG = 0x1C 
-    
-    FIFO_EN = 0x23
-    
-    I2C_MST_CTRL = 0x24 
-    I2C_SLV0_ADDR = 0x25 
-    I2C_SLV0_REG = 0x26
-    I2C_SLV0_CTRL = 0x27
-    I2C_SLV1_ADDR = 0x28
-    I2C_SLV1_REG = 0x29
-    I2C_SLV1_CTRL = 0x2A
-    I2C_SLV2_ADDR = 0x2B
-    I2C_SLV2_REG = 0x2C
-    I2C_SLV2_CTRL = 0x2D
-    I2C_SLV3_ADDR = 0x2E
-    I2C_SLV3_REG = 0x2F
-    I2C_SLV3_CTRL = 0x30
-    I2C_SLV4_ADDR = 0x31
-    I2C_SLV4_REG = 0x32
-    I2C_SLV4_DO = 0x33
-    I2C_SLV4_CTRL = 0x34
-    I2C_SLV4_DI = 0x35
-    I2C_MST_STATUS = 0x36 
-    
-    INT_PIN_CFG = 0x37
-    INT_ENABLE = 0x38
-    INT_STATUS = 0x3A
-    
-    ACCEL_XOUT_H = 0x3B
-    ACCEL_XOUT_L = 0x3C
-    ACCEL_YOUT_H = 0x3D
-    ACCEL_YOUT_L = 0x3E
-    ACCEL_ZOUT_H = 0x3F
-    ACCEL_ZOUT_L = 0x40
-    TEMP_OUT_H = 0x41
-    TEMP_OUT_L = 0x42
-    GYRO_XOUT_H = 0x43
-    GYRO_XOUT_L = 0x44
-    GYRO_YOUT_H = 0x45
-    GYRO_YOUT_L = 0x46
-    GYRO_ZOUT_H = 0x47
-    GYRO_ZOUT_L = 0x48
-    
-    EXT_SENS_DATA_00 = 0x49
-    EXT_SENS_DATA_01 = 0x4A
-    EXT_SENS_DATA_02 = 0x4B
-    EXT_SENS_DATA_03 = 0x4C
-    EXT_SENS_DATA_04 = 0x4D
-    EXT_SENS_DATA_05 = 0x4E
-    EXT_SENS_DATA_06 = 0x4F
-    EXT_SENS_DATA_07 = 0x50
-    EXT_SENS_DATA_08 = 0x51
-    EXT_SENS_DATA_09 = 0x52
-    EXT_SENS_DATA_10 = 0x53
-    EXT_SENS_DATA_11 = 0x54
-    EXT_SENS_DATA_12 = 0x55
-    EXT_SENS_DATA_13 = 0x56
-    EXT_SENS_DATA_14 = 0x57
-    EXT_SENS_DATA_15 = 0x58
-    EXT_SENS_DATA_16 = 0x59
-    EXT_SENS_DATA_17 = 0x5A
-    EXT_SENS_DATA_18 = 0x5B
-    EXT_SENS_DATA_19 = 0x5C
-    EXT_SENS_DATA_20 = 0x5D
-    EXT_SENS_DATA_21 = 0x5E
-    EXT_SENS_DATA_22 = 0x5F
-    EXT_SENS_DATA_23 = 0x60
-    
-    I2C_SLV0_DO = 0x63
-    I2C_SLV1_DO = 0x64
-    I2C_SLV2_DO = 0x65
-    I2C_SLV3_DO = 0x66
-    
-    I2C_MST_DELAY_CTRL = 0x67
-    SIGNAL_PATH_RESET = 0x68
-    USER_CTRL = 0x6A
-    
-    PWR_MGMT_1 = 0x6B
-    PWR_MGMT_2 = 0x6C
-    
-    FIFO_COUNTH = 0x72
-    FIFO_COUNTL = 0x73
-    FIFO_R_W = 0x74
-    WHO_AM_I = 0x75
-    
-    GYRO_LSB = {
-        0 : 131,
-        1 : 65.5,
-        2 : 32.8,
-        3 : 16.4}
-    
-    ACCEL_LSB = {
-        0 : 16384,
-        1 : 8192,
-        2 : 4096,
-        3 : 2048}
+    CRA = 0x00
+    CRB = 0X01
+    MR = 0X02
+    DXRA = 0X03
+    DXRB = 0X04
+    DYRA = 0X05
+    DYRB = 0X06
+    DZRA = 0X07
+    DZRB = 0X08
+    SR = 0X09
+    IRA = 0X0A
+    IRB = 0X0B
+    IRC = 0X0C
+
+    sample_average = {
+        1 : "00";
+        2 : "01";
+        4 : "10";
+        8 : "11";
+    }
+
+    output_rate = {
+        0.75 : "000";
+        1.5 : "001";
+        3 : "010";
+        7.5 : "011";
+        15 : "100";
+        30 : "101";
+        75 : "110";
+    }
+
+    sensor_range = {
+        0.88 : ["000", 1370];
+        1.3 : ["001", 1090];
+        1.9 : ["010", 820];
+        2.5 : ["011", 660];
+        4.0 : ["100", 440];
+        4.7 : ["101", 390];
+        5.6 : ["1101", 330];
+        8.1 : ["111", 230];
+    }
--- example.py
+++ example.py
@@ -5,8 +5,8 @@ Created on Sun Feb 25 16:57:55 2024
 @author: EugenioCalandrini
 """
 
-from mpu6050 import MPU6050
-from register_map import RegisterMap
+from MPU6050.mpu6050 import MPU6050
+from MPU6050.register_map import RegisterMap
 import numpy as np
 import time
 
